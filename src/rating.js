4ff456e7dba4cd62c175e1022f32f7d092a610eb
wendyhamel <whamel@it4web.nl>
2022-11-01 12:54:54 +0100

    Added gradient, separated tailwindcss, alpine.js and html.

---
+++ src/rating.js
@@ -0,0 +1,14 @@
+window.rating = function() {
+    return {
+        rates: [
+            { number: 1},
+            { number: 2},
+            { number: 3},
+            { number: 4},
+            { number: 5}
+        ],
+        picked : false,
+        start : true,
+        submit : false,
+    }
+}
---
+++ src/tailwind-config.js
@@ -0,0 +1,15 @@
+tailwind.config = {
+    theme: {
+        fontFamily: {
+            'sans': ['Overpass', 'sans-serif'],
+        },
+        colors: {
+            orange: 'hsl(25, 97%, 53%)',
+            white: 'hsl(0, 0%, 100%)',
+            'light-gray': 'hsl(217, 12%, 63%)',
+            gray: 'hsl(216, 12%, 54%)',
+            blue: 'hsl(213, 19%, 18%)',
+            'dark-blue': 'hsl(216, 12%, 8%)',
+        }
+    }
+}
